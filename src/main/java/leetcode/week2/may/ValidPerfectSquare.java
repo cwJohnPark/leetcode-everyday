b2a88dd26e8545507f2cea1d4efafc13647cdbd5
cwJohnPark <pierrotcw@gmail.com>
2020-05-10 11:13:04 +0900

    Init repository

---
+++ src/main/java/leetcode/week2/may/ValidPerfectSquare.java
@@ -0,0 +1,44 @@
+package leetcode.week2.may;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *  양의 정수 num이 주어졌을 때, num이 완전한 정사각형이라면 true를, 아니라면 false를 반환하시오.
+ */
+public class ValidPerfectSquare {
+    public static void main(String[] args) {
+        ValidPerfectSquare square = new ValidPerfectSquare();
+        assert square.isPerfectSquare(Integer.MAX_VALUE) == false;
+        assert square.isPerfectSquare(16) == true;
+        System.out.println(square.isPerfectSquare(4));
+    }
+    public boolean isPerfectSquare(int num) {
+        if(num == 1) return true;
+        int low = 1;
+        int high = num / 2;
+        while(true) {
+            int guess = getGuess(low, high);
+
+            long guessSquared = Integer.MAX_VALUE < (long) guess * guess ? Long.MAX_VALUE : guess * guess;
+            if(guessSquared == num) {
+                return true;
+            } else if(high == guess) {
+                return false;
+            }
+
+            if(guessSquared > num) {
+                high = guess;
+            } else {
+                low = guess;
+            }
+
+        }
+    }
+
+    private int getGuess(int low, int high) {
+        return (int) (Math.round((double)(low + high) / 2));
+    }
+}
